d8ab0e8a7d22cdba2ce62372968765a36d2fdf86
danieldjupvik <daniel_22@icloud.com>
2026-01-02 18:43:34 +0100

    Update API URL to use new hosted service and remove CORS configuration

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,9 @@
 export const authUrl = '/auth/local';
 export const placesUrl = '/places';
 
-//Cors
-const cors = 'https://noroffcors.herokuapp.com/';
-
 //API urls
 const localApiUrl = 'http://localhost:1337';
 
-const hostedApiUrl = cors + 'https://holidaze-strapi.herokuapp.com';
+const hostedApiUrl = 'https://holidaze-strapi.onrender.com';
 
 export const baseUrl = hostedApiUrl;
